11aef10963679b30e2b7b1a041351c99c33a13a4
胡凡 <hufan.Akarin@Gmail.com>
2016-02-25 22:40:37 +0800

    pretty code

--- music163_get_lyric_json.py
+++ music163_get_lyric_json.py
@@ -3,13 +3,15 @@
 
 import sys
 import urllib2
-    
+
+
 def get_lyric(music_id):
     url = "http://music.163.com/api/song/lyric?os=osx&id=" + str(music_id) + "&lv=-1&kv=-1&tv=-1"
     opener = urllib2.build_opener()
-    opener.addheaders=[('Cookie', 'appver=1.5.2'),('Referer','http://music.163.com/')]
+    opener.addheaders = [('Cookie', 'appver=1.5.2'), ('Referer', 'http://music.163.com/')]
     result = opener.open(url)
     return result.read()
-    
+
+
 if __name__ == '__main__':
     print(get_lyric(sys.argv[1]))
